6daf54671e165a6ff0e3ea8aca3c23b14494cbf1
mezianefraga00 <mezianefraga00@gmail.com>
2021-03-09 23:24:29 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(){
+}
+function subtract(a,b){
+    return a-b;
+}
+function add(a,b){
+    return a+b;
+}
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+function increment(n){
+    return n+= 1;  
+}
+function decrement(n){
+    return n-= 1;  
+}
+function makeInt(n){
+    let base=10;
+    return parseInt(n,base);
+}
+function preserveDecimal(n){
+
+ return   parseFloat(n);
+}
